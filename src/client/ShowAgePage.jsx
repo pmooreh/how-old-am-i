3043af97d72f8a9bbfdc125ed2b4aa266c3f437a
Patrick Moore <phatrickmoore@gmail.com>
2019-03-08 01:45:57 -0600

    fixing broken link

--- src/client/ShowAgePage.jsx
+++ src/client/ShowAgePage.jsx
@@ -88,7 +88,7 @@ class ShowAgePage extends React.PureComponent {
           </div>
         </div>
         <div className="has-text-centered">
-          <a href={link}>
+          <a href="www.actualage.org">
             Share me with a friend.
           </a>
         </div>
